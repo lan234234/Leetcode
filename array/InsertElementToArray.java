345dc6f35f913b96864a80fd2822a2da08c7f355
Lan <lan.peng234@gmail.com>
2024-01-06 17:03:14 -0700

    update InsertElementToArray

--- array/InsertElementToArray.java
+++ array/InsertElementToArray.java
@@ -6,24 +6,33 @@ return a new array with the element inserted
 * */
 
 public class InsertElementToArray {
-    public int[] insertElementToArray(int[] array, int[] insertArray) {
+
+    public int[] insertElementToArray(int[] array, int[] element) {
         //corner case
-        if (insertArray == null || insertArray.length < 2 || insertArray[1] < 0) return array;
-        if (array == null) {
-            if (insertArray[1] != 0) return array;
-            return new int[]{insertArray[0]};
+        //1. element is invalid
+        if (element == null || element.length != 2 || element[1] < 0)
+            return array;
+        //2. array is null or empty
+        if (array == null || array.length == 0) {
+            if (element[1] == 0)
+                return new int[]{element[0]};
+            return array;
         }
-        if (insertArray[1] > array.length) return array;
+        //3. position is invalid
+        if (array.length < element[1])
+            return array;
+
         //general case
         int[] result = new int[array.length + 1];
-        for (int i = 0, j = 0; j < array.length + 1; j++) {
-            if (j != insertArray[1]) {
+        for (int i = 0, j = 0; j < result.length; j++) {
+            if (j == element[1]) {
+                result[j] = element[0];
+            } else {
                 result[j] = array[i];
                 i++;
-            } else {
-                result[j] = insertArray[0];
             }
         }
         return result;
     }
+
 }
