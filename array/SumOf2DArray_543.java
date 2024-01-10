419430d9c944696bc474305d5441e27baad65481
Lan <lan.peng234@gmail.com>
2024-01-09 22:29:18 -0700

    SumOf2DArray

---
+++ array/SumOf2DArray_543.java
@@ -0,0 +1,29 @@
+package array;
+
+/*
+Given a two-dimensional array, calculate the sum of all its elements
+ */
+
+public class SumOf2DArray_543 {
+    public int sum(int[][] array) {
+    /*
+    assumption: int[]
+    input: int[]
+    output: int
+    corner case:
+    array == null || array.length == 0 || array[0].length == 0
+    */
+        int sum = 0;
+        for (int r = 0; r < array.length; r++) {
+            sum += sumRow(array[r]);
+        }
+        return sum;
+    }
+    private int sumRow(int[] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
