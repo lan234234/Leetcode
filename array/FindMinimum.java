5e40e7f3497a651449e8470248c702f2725f844e
Lan <lan.peng234@gmail.com>
2024-01-09 22:21:46 -0700

    FindMinimum

---
+++ array/FindMinimum.java
@@ -0,0 +1,19 @@
+package array;
+
+/*
+Given an array, return its minimum value
+ */
+
+public class FindMinimum {
+    public int findMinimum(int[] array) {
+        //corner case:
+        if (array == null || array.length == 0)
+            return 0;
+        //general case:
+        int min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            min = min > array[i] ? array[i] : min;
+        }
+        return min;
+    }
+}
