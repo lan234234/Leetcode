da8f473383971000247401e85fbf83da59a49187
Lan <lan.peng234@gmail.com>
2024-01-09 22:27:46 -0700

    ReverseArray

---
+++ array/ReverseArray_542.java
@@ -0,0 +1,26 @@
+package array;
+
+/*
+Given an array, reverse all its elements.
+ */
+
+public class ReverseArray_542 {
+    public void reverse(int[] array) {
+    /*
+    assumption: int[]
+    input: int[]
+    output: void
+    corner case:
+    array == null || array.length <= 0
+    */
+        for (int i = 0, j = array.length - 1; i < j; i++, j--) {
+            swap(array, i, j);
+        }
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
