bab1a205fb8be96d5e2c1c33e76581b4288ad02d
Lan <lan.peng234@gmail.com>
2024-01-09 22:26:18 -0700

    SwapTwoElement

---
+++ array/SwapTwoElement_541.java
@@ -0,0 +1,20 @@
+package array;
+
+/*
+Given an array, and two indices i and j.
+Swap the element at position i and element at position j.
+*/
+public class SwapTwoElement_541 {
+    public void swapTwoElement(int[] array, int i, int j) {
+        // assume: int array
+        // input: int[]
+        // output: int[]
+    /* corner case
+    array == null || array.length == 0
+    i or j is/are invalid
+    */
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
