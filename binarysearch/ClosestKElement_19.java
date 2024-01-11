e1d4598b1ac891b55ed4b5b4f0d8eefd11258a1e
Lan <lan.peng234@gmail.com>
2024-01-11 11:43:29 -0700

    ClosestKElement

--- binarysearch/ClosestKElement_19.java
+++ binarysearch/ClosestKElement_19.java
@@ -1,64 +1,70 @@
 package binarysearch;
 
+/*
+Given a target T, an K and an array A,
+find the K closest numbers to T in A.
+
+
+
+assume:
+array: int[], ordered in ascending order
+target: int
+k: int
+tie? smaller one is preferred
+
+ */
+
 public class ClosestKElement_19 {
     public int[] closestKElement(int[] array, int target, int k) {
-        //corner case:
-        if (array == null || array.length == 0 ||	k > array.length || k <=0) {
+        // corner case:
+        // 1. array is invalid
+        if (array == null || array.length == 0) {
             return new int[0];
         }
+        // 2. k is invalid
+        if (k <= 0 || k > array.length) {
+            return new int[0];
+        }
+
         //general case:
-        int index = closestElement(array, target);
+        //step1: find the closest number by binary search
+        int j = closestNumber(array, target);		//traverse array forward
+        //step2: traverse and compare to find Kth elements
+        int i = j - 1;					//traverse array backward
         int[] result = new int[k];
-        result[0] = array[index];
-        int m = 1;
-        int i = index - 1;
-        int j = index + 1;
-        while (m < k && i >= 0 && j < array.length) {
-            if (Math.abs(array[i] - target) <= Math.abs(array[j] - target)) {
-                result[m] = array[i];
-                i--;
-                m++;
+        int index = 0;					//traverse result
+        while (index < k) {
+            if (j == array.length ||
+                    (i >= 0 && Math.abs(array[i] - target) <= Math.abs(array[j] - target))) {
+                result[index++] = array[i--];
             } else {
-                result[m] = array[j];
-                j++;
-                m++;
-            }
-        }
-        if (i < 0) {
-            while (m < k) {
-                result[m] = array[j];
-                j++;
-                m++;
-            }
-        }
-        if (j == array.length) {
-            while (m < k) {
-                result[m] = array[i];
-                i--;
-                m++;
+                result[index++] = array[j++];
             }
         }
         return result;
     }
 
-    private int closestElement(int[] array, int target) {
+    private int closestNumber(int[] array, int target) {
         int left = 0;
         int right = array.length - 1;
         while (left < right - 1) {
             int mid = left + (right - left) / 2;
-            if (array[mid] < target) {
-                left = mid;
-            } else if (array[mid] > target) {
+            if (array[mid] > target) {
                 right = mid;
+            } else if (array[mid] < target) {
+                left = mid;
             } else {
                 return mid;
             }
         }
-        if (Math.abs(array[left] - target) <= Math.abs(array[right] - target)) {
-            return left;
-        }
-        return right;
+        return Math.abs(array[left] - target) <= Math.abs(array[right] - target) ?
+                left : right;
     }
-
-
 }
+
+/*
+time complexity: O(logn + k)
+    step1: O(logn)
+    step2: O(k)
+space complexity: O(1)
+ */
