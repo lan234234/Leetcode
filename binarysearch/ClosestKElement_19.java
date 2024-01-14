9160b3d82349eb74d5d6256b62b188c272a1fb64
Lan <lan.peng234@gmail.com>
2024-01-13 22:28:39 -0700

    ClosestKElement

--- binarysearch/ClosestKElement_19.java
+++ binarysearch/ClosestKElement_19.java
@@ -15,6 +15,9 @@ tie? smaller one is preferred
  */
 
 public class ClosestKElement_19 {
+    /*
+    / method 1:
+     */
     public int[] closestKElement(int[] array, int target, int k) {
         // corner case:
         // 1. array is invalid
@@ -60,7 +63,7 @@ public class ClosestKElement_19 {
         return Math.abs(array[left] - target) <= Math.abs(array[right] - target) ?
                 left : right;
     }
-}
+
 
 /*
 time complexity: O(logn + k)
@@ -68,3 +71,74 @@ time complexity: O(logn + k)
     step2: O(k)
 space complexity: O(1)
  */
+
+    /*
+    / method 2:
+     */
+    public int[] kthClosestElement(int[] array, int target, int k) {
+        //corner case:
+        if (array == null || array.length == 0) {
+            return new int[0];
+        }
+        if (k <= 0 || k > array.length) {
+            return new int[0];
+        }
+        //step1: binary search to find the closest number index
+        int index = findClosest(array, target);
+        if (k == 1)		return new int[]{array[index]};
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = helper(array, index + 1, index, target, i + 1);
+        }
+        return result;
+    }
+
+    private int findClosest(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right - 1) {
+            int mid = left + (right - left) / 2;
+            if (array[mid] > target) {
+                right = mid;
+            } else if (array[mid] < target) {
+                left = mid;
+            } else {
+                return mid;
+            }
+        }
+        return Math.abs(array[left] - target) <= Math.abs(array[right] - target) ? left : right;
+    }
+
+    private int helper(int[] array, int i, int j, int target, int k) {
+        //corner case:
+        if (i <= 0) {
+            return array[j + k];
+        }
+        if (j >= array.length - 1) {
+            return array[i - k];
+        }
+        if (k == 1) {
+            return Math.abs(array[i - 1] - target) <= Math.abs(array[j + 1] - target) ?
+                    array[i - 1] : array[j + 1];
+        }
+        i -= k / 2;
+        j += k / 2;
+        if (i < 0) {
+            return helper(array, i + k / 2, j, target, k - k / 2);
+        }
+        if (j > array.length - 1) {
+            return helper(array, i, j - k / 2, target, k - k / 2);
+        }
+        if (Math.abs(array[i] - target) <= Math.abs(array[j] - target)) {
+            return helper(array, i, j - k / 2, target, k - k / 2);
+        }
+        return helper(array, i + k / 2, j, target, k - k / 2);
+    }
+
+    /*
+    time complexity: O(logn + klogk)
+        step1: O(logn)
+        step2: O(klogk)
+    space complexity: O(klogk)
+     */
+}
