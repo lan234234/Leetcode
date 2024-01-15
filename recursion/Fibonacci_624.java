f2947b6ecce5b5f229dbbb5c20ba4a292af00622
Lan <lan.peng234@gmail.com>
2024-01-14 22:12:32 -0700

    Fibonacci

---
+++ recursion/Fibonacci_624.java
@@ -0,0 +1,33 @@
+package recursion;
+
+/*
+Get the Kth number in the Fibonacci Sequence.
+(K is 0-indexed, the 0th Fibonacci number is 0 and the 1st Fibonacci number is 1).
+
+
+assumption
+f(n) = f(n - 1) + f(n - 2)
+f(0) = 0
+f(1) = 1
+if n < 0		return 0
+overflow???
+
+ */
+
+public class Fibonacci_624 {
+    public int fibonacci(int n) {
+        //corner case:
+        if (n < 0) {
+            return 0;
+        }
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        //general case:
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+}
