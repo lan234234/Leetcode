eb63131d369f81ba7f70535c803157bb0c1e5b2a
Lan <lan.peng234@gmail.com>
2024-01-15 17:32:22 -0700

    SelectionSort

---
+++ sorting/SelectionSort_4.java
@@ -0,0 +1,32 @@
+package sorting;
+
+public class SelectionSort_4 {
+    public int[] selectionSort(int[] array) {
+        //corner case:
+        if (array == null || array.length <= 1) {
+            return array;
+        }
+//general case:
+        for (int i = 0; i < array.length - 1; i++) {
+            //find the min in the array in the range of [i, array.length - 1]
+            int minIndex = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            swap(array, i, minIndex);
+        }
+        return array;
+    }
+
+    private void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+//    time complexity: O(n^2)
+//    space complexity: O(1)
+
+}
