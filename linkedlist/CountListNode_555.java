2646b0fe090d269d864c0db10dbad3e52e6a8a48
Lan <lan.peng234@gmail.com>
2024-01-19 12:05:30 -0700

    CountListNode

---
+++ linkedlist/CountListNode_555.java
@@ -0,0 +1,35 @@
+package linkedlist;
+
+/*
+Given a linked list, count the number of nodes in it.
+
+clarify:
+implement ListNode class?
+valid no cycle single linkedList
+if head == null		return 0
+
+input: ListNode head
+output: int
+ */
+
+public class CountListNode_555 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        //constructor
+        ListNode(int n) {
+            val = n;
+        }
+    }
+    public int countListNode(ListNode head) {
+        int count = 0;
+        while( head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+//    time complexity: O(n)
+//    space complexity: O(1)
+
+}
