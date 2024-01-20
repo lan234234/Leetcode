438b4d82baaa3b3e221ae1a6163d243bf83ba7b4
Lan <lan.peng234@gmail.com>
2024-01-19 22:09:36 -0700

    RemoveNodeInLinkedList

---
+++ linkedlist/RemoveNodeInLinkedList_414.java
@@ -0,0 +1,41 @@
+package linkedlist;
+
+/*
+Remove all elements from a linked list of integers that have value val.
+
+assume: single linkedList without cycle
+
+ */
+
+public class RemoveNodeInLinkedList_414 {
+    public class ListNode {
+        int value;
+        ListNode next;
+        // constructor
+        ListNode(int n) {
+            value = n;
+        }
+    }
+    public ListNode removeNodeInLinkedList(ListNode head, int val) {
+        //corner case:
+        if (head == null) {
+            return head;
+        }
+    //general case:
+        ListNode dummy = new ListNode(-1);
+        ListNode pre = dummy;
+        dummy.next = head;
+        while (head != null) {
+            if (head.value == val) {
+                pre.next = head.next;
+            } else {
+                pre = head;
+            }
+            head = head.next;
+        }
+        return dummy.next;
+    }
+//    time complexity: O(n)
+//    space complexity: O(1)
+
+}
