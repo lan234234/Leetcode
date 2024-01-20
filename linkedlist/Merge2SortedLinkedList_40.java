4619d2a875fb963c3501c354936fdb742f13130a
Lan <lan.peng234@gmail.com>
2024-01-19 21:49:00 -0700

    Merge2SortedLinkedList

---
+++ linkedlist/Merge2SortedLinkedList_40.java
@@ -0,0 +1,45 @@
+package linkedlist;
+
+/*
+Merge two sorted lists into one large sorted list.
+
+
+
+clarify:
+assume: single linkedlist without cycle
+
+input: ListNode one, ListNode two
+output: ListNode
+
+ */
+
+public class Merge2SortedLinkedList_40 {
+    public class ListNode {
+        int value;
+        ListNode next;
+        // constructor
+        ListNode(int n) {
+            value = n;
+        }
+    }
+
+    public ListNode merge2SortedLinkedList(ListNode one, ListNode two) {
+        ListNode dummy = new ListNode(-1);
+        ListNode pre = dummy;
+        while (one != null && two != null) {
+            if (one.value <= two.value) {
+                pre.next = one;
+                one = one.next;
+            } else {
+                pre.next = two;
+                two = two.next;
+            }
+            pre = pre.next;
+        }
+        pre.next = one == null ? two : one;
+        return dummy.next;
+    }
+//    time complexity: O(m + n)
+//    space complexity: O(1)
+
+}
