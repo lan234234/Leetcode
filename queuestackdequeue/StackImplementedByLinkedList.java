177aae546d44ef34fe349e184fc8a05c1a43b7dc
Lan <lan.peng234@gmail.com>
2024-01-29 13:07:08 -0700

    StackImplementedByLinkedList

---
+++ queuestackdequeue/StackImplementedByLinkedList.java
@@ -0,0 +1,38 @@
+package queuestackdequeue;
+
+import linkedlist.ListNode;
+
+public class StackImplementedByLinkedList {
+    // member field:
+    int size;
+    ListNode head;
+
+    // API:
+    public int size() {
+        return size;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public boolean push(Integer n) {
+        ListNode newHead = new ListNode(n);
+        newHead.next = head;
+        size++;
+        head = newHead;
+        return true;
+    }
+    public Integer pop() {
+        if (head == null) {
+            return null;
+        }
+        ListNode result = head;
+        head = head.next;
+        result.next = null;
+        size--;
+        return result.value;
+    }
+    public Integer top() {
+        return head.value;
+    }
+
+}
