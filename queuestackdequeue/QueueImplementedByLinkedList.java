c16ec6288e1b93ae0b99e03608f073e80377dff2
Lan <lan.peng234@gmail.com>
2024-01-29 17:19:05 -0700

    QueueImplementedByLinkedList

---
+++ queuestackdequeue/QueueImplementedByLinkedList.java
@@ -0,0 +1,44 @@
+package queuestackdequeue;
+
+import linkedlist.ListNode;
+
+public class QueueImplementedByLinkedList {
+    // field:
+    ListNode head;	// point to the first element
+    ListNode tail;		// point to the last element
+    int size;		// store the size of the linked list
+
+    // API
+    public int size() {
+        return size;
+    }
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    public boolean offer(Integer n) {
+        ListNode newTail = new ListNode(n);
+        size++;
+        if (this.isEmpty()) {
+            head = newTail;
+            tail = newTail;
+        } else {
+            tail.next = newTail;
+            tail = newTail;
+        }
+        return true;
+    }
+    public Integer peek() {
+        return this.isEmpty() ?  null : head.value;
+    }
+    public Integer poll() {
+        if (this.isEmpty()) {
+            return null;
+        }
+        ListNode result = head;
+        head = head.next;
+        result.next = null;
+        size--;
+        return result.value;
+    }
+
+}
