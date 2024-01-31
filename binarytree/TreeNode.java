2fb1a61ed05615085ae06f998ef82fa0c4d132a6
Lan <lan.peng234@gmail.com>
2024-01-31 10:15:56 -0700

    TreeNode

---
+++ binarytree/TreeNode.java
@@ -0,0 +1,13 @@
+package binarytree;
+
+public class TreeNode {
+    // field
+    public int key;
+    public TreeNode left;
+    public TreeNode right;
+    // constructor
+    public TreeNode(int key) {
+        this.key = key;
+    }
+
+}
