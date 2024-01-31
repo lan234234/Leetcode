ff36b2f5dcf11636c9fca2fd5e87b162c55cc5f5
Lan <lan.peng234@gmail.com>
2024-01-31 10:46:25 -0700

    RecursivelyTraverse

--- binarytree/RecursivelyTraverse.java
+++ binarytree/RecursivelyTraverse.java
@@ -21,7 +21,46 @@ public class RecursivelyTraverse {
         result.add(root.key);
         preOrderHelper(root.left, result);
         preOrderHelper(root.right, result);
-        return;
     }
 
+    /**
+     * 654. in-order traverse
+     */
+    public List<Integer> inOrderTraverse(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inOrderHelper(root, result);
+        return result;
+    }
+    private void inOrderHelper(TreeNode root, List<Integer> result) {
+        // base case:
+        if (root == null) {
+            return;
+        }
+        // recursive rule:
+        inOrderHelper(root.left, result);
+        result.add(root.key);
+        inOrderHelper(root.right, result);
+    }
+
+    /**
+     * 656. in-order traverse
+     */
+    public List<Integer> postOrderTraverse(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postOrderHelper(root, result);
+        return result;
+    }
+    private void postOrderHelper(TreeNode root, List<Integer> result) {
+        // base case:
+        if (root == null) {
+            return;
+        }
+        // recursive rule:
+        postOrderHelper(root.left, result);
+        postOrderHelper(root.right, result);
+        result.add(root.key);
+    }
 }
+//TC: O(n)
+//SC: O(height) (average case: O(logn) worst case: O(n))
+
