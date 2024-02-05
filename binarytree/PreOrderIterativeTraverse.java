18aee165082b02db8c4a226e300168ead7542fe2
Lan <lan.peng234@gmail.com>
2024-02-05 16:13:06 -0700

    PreOrderIterativeTraverse

---
+++ binarytree/PreOrderIterativeTraverse.java
@@ -0,0 +1,28 @@
+package binarytree;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class PreOrderIterativeTraverse {
+    public List<Integer> preOrderIterativeTraverse(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.offerFirst(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pollFirst();
+            list.add(cur.key);
+            if (cur.right != null) {
+                stack.offerFirst(cur.right);
+            }
+            if (cur.left != null) {
+                stack.offerFirst(cur.left);
+            }
+        }
+        return list;
+    }
+}
