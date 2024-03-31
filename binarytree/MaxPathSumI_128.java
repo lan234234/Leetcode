1bea2e0c70c4651cceef25ecfeb79ba8234729d8
Lan <lan.peng234@gmail.com>
2024-03-30 22:05:09 -0700

    MaxPathSumI

---
+++ binarytree/MaxPathSumI_128.java
@@ -0,0 +1,27 @@
+package binarytree;
+
+/**
+ * Given a binary tree in which each node contains an integer number.
+ * Find the maximum possible sum from any node to any node
+ */
+public class MaxPathSumI_128 {
+    public int maxPathSum(TreeNode root) {
+        // assume all sum are in the integer range
+        // assume root is not null
+        int[] max = new int[]{root.key};
+        helper(root, max);
+        return max[0];
+    }
+    // update maxSum and return current max sum of a single path containing the root
+    public int helper(TreeNode root, int[] max) {
+        if (root == null) return 0;
+        int leftResult = helper(root.left, max);
+        int rightResult = helper(root.right, max);
+        //if leftResult or rightResult < 0, it's no need the inherit
+        leftResult = leftResult > 0 ? leftResult : 0;
+        rightResult = rightResult > 0 ? rightResult : 0;
+        max[0] = Math.max(max[0], leftResult + rightResult + root.key);
+        return Math.max(leftResult, rightResult) + root.key;
+    }
+
+}
