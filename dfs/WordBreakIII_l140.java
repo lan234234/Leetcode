d6f66849e44ba8fd942bc9164f14168b6a9bb229
Lan <lan.peng234@gmail.com>
2024-04-16 20:59:36 -0700

    WordBreakIII

--- dfs/WordBreakIII_l140.java
+++ dfs/WordBreakIII_l140.java
@@ -1,11 +1,21 @@
-package dp;
+package dfs;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Given a string s and a dictionary of strings wordDict,
+ * add spaces in s to construct a sentence where each word is a valid dictionary word.
+ * Return all such possible sentences in any order.
+ *
+ * Note that the same word in the dictionary may be reused multiple times in the segmentation.
+ */
 public class WordBreakIII_l140 {
+    /**
+     * dp
+     */
     public List<String> wordBreak(String s, List<String> dict) {
         // assume s is not null and empty
         // assume dict is not null and empty
@@ -60,5 +70,46 @@ public class WordBreakIII_l140 {
         }
         return result;
     }
+
+    /**
+     * dfs
+     */
+    public List<String> wordBreak2(String s, List<String> dict) {
+        // assume s is not null and empty
+        // assume dict is not null and empty
+        // assume every string in dict is not null and empty
+        StringBuilder sb = new StringBuilder();		// record current word combination
+        List<String> result = new ArrayList<>();
+        // convert the dict list to a set
+        Set<String> set = toSet(dict);
+        helper(s, set, 0, 0, sb, result);
+        return result;
+    }
+    // determine whether to cut the substring starting at startIndex in current index
+    // if cut, the substring will be separated to 2 parts: [startIndex, index] and (index, input.length()))
+    public void helper(String input, Set<String> set, int startIndex, int index, StringBuilder sb, List<String> result) {
+        // termination condition: when we finish determining all index
+        if (index == input.length()) {
+            return;
+        }
+        // choose not to cut in current index
+        helper(input, set, startIndex, index + 1, sb, result);
+        // choose to cut
+        String subS = input.substring(startIndex, index + 1);
+        if (set.contains(subS)) {
+            int size = sb.length();
+            if (startIndex != 0) {
+                sb.append(" ");
+            }
+            sb.append(subS);
+            if (index == input.length() - 1) {
+                result.add(sb.toString());
+            }
+            helper(input, set, index + 1, index + 1, sb, result);
+            sb.delete(size, sb.length());
+        }
+    }
+
+
 
 }
