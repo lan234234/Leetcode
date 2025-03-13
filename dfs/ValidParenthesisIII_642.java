e9ef3c7c31a0b0397a4d9f632dd160ee48f918f4
Lan <lan.peng234@gmail.com>
2025-03-13 12:25:12 -0700

    642

--- dfs/ValidParenthesisIII_642.java
+++ dfs/ValidParenthesisIII_642.java
@@ -1,4 +1,47 @@
 package dfs;
 
+import java.util.*;
+
 public class ValidParenthesisIII_642 {
+    char[] p;
+    int[] count;
+    public List<String> validParenthesesIII(int l, int m, int n) {
+        List<String> res = new ArrayList<>();
+        count = new int[]{l, m, n};
+        p = new char[]{'(', '<', '{', ')', '>', '}'};
+        dfs((l + m + n) * 2, new StringBuilder(), res, new ArrayDeque<>());
+        return res;
+    }
+
+    private void dfs(int remain, StringBuilder sb, List<String> res, Deque<Integer> stack) {
+        if (remain == 0) {
+            res.add(sb.toString());
+            return;
+        }
+
+        // case 1: left
+        Integer top = stack.peekFirst();
+        top = top == null ? 3 : top;
+        for (int i = 0; i < top; i++) {
+            if (count[i] > 0) {
+                sb.append(p[i]);
+                count[i]--;
+                stack.offerFirst(i);
+                dfs(remain - 1, sb, res, stack);
+                sb.deleteCharAt(sb.length() - 1);
+                count[i]++;
+                stack.pollFirst();
+            }
+        }
+
+        // case 2: right
+        if (top != 3) {
+            stack.pollFirst();
+            int right = top + 3;
+            sb.append(p[right]);
+            dfs(remain - 1, sb, res, stack);
+            sb.deleteCharAt(sb.length() - 1);
+            stack.offerFirst(top);
+        }
+    }
 }
