926f78c7faf6b5ebcc1872c535b333e8f477ac13
Lan <lan.peng234@gmail.com>
2025-05-01 12:20:01 -0700

    SegmentTree

---
+++ segmentTree/SegmentTree.java
@@ -0,0 +1,59 @@
+package segmentTree;
+
+public class SegmentTree {
+    int[] tree;
+    int n;
+
+    public SegmentTree(int[] arr) {
+        n = arr.length;
+        tree = new int[4 * n];
+        build(arr, 0, 0, n - 1);
+    }
+
+    private void build(int[] arr, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+        } else {
+            int mid = (start + end) / 2;
+            build(arr, 2 * node + 1, start, mid);
+            build(arr, 2 * node + 2, mid + 1, end);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    public void update(int index, int value) {
+        update(0, 0, n - 1, index, value);
+    }
+
+    private void update(int node, int start, int end, int index, int value) {
+        if (start == end) {
+            tree[node] = value;
+        } else {
+            int mid = (start + end) / 2;
+            if (index <= mid) {
+                update(2 * node + 1, start, mid, index, value);
+            } else {
+                update(2 * node + 2, mid + 1, end, index, value);
+            }
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    public int query(int left, int right) {
+        return query(0, 0, n - 1, left, right);
+    }
+
+    private int query(int node, int start, int end, int left, int right) {
+        if (right < start || end < left) {
+            return 0; // No overlap
+        }
+        if (left <= start && end <= right) {
+            return tree[node]; // Total overlap
+        }
+        // Partial overlap
+        int mid = (start + end) / 2;
+        int sumLeft = query(2 * node + 1, start, mid, left, right);
+        int sumRight = query(2 * node + 2, mid + 1, end, left, right);
+        return sumLeft + sumRight;
+    }
+}
