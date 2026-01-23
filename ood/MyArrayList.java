fb6538206a72c98728c74a11e97d20e490c81ac9
Lan <lan.peng234@gmail.com>
2026-01-22 17:30:51 -0700

    design ArrayList

--- ood/MyArrayList.java
+++ ood/MyArrayList.java
@@ -1,24 +1,88 @@
 package ood;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 
-public class MyArrayList<T> {
-    T[] elements;
+public class MyArrayList {
+    private static final int DEFAULT_CAPACITY = 10;
+    private static final int RESIZE_FACTOR = 2;
 
+    private int[] array;
+    private int size;
 
     public MyArrayList() {
-        this.elements = (T[]) new Object[2];
-        elements[0] = (T)Integer.valueOf(12);
-        elements[1] = (T)"Hello";
-
+        this(DEFAULT_CAPACITY);
     }
 
-//    public static void main(String[] args) {
-//        MyArrayList<String> a = new MyArrayList<>();
-//        System.out.println("start");
-//        Integer i = 12;
-////        String s = (String) i;
-////        System.out.println(a.elements[0]);
-//        ArrayList<String> s = new ArrayList<>();
-//    }
+    public MyArrayList(int capacity) {
+        if (capacity < 0) throw new IllegalArgumentException("capacity < 0");
+        array = new int[Math.max(capacity, DEFAULT_CAPACITY)];
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int get(int index) {
+        validIndex(index);
+        return array[index];
+    }
+
+    public int set(int index, int val) {
+        validIndex(index);
+        int oldVal = array[index];
+        array[index] = val;
+        return oldVal;
+    }
+
+    public boolean add(int val) {
+        return add(size, val);
+    }
+
+    public boolean add(int index, int val) {
+        validIndexForAdd(index);
+
+        if (size == array.length) {
+            resize();
+        }
+
+        // shift elements to the right
+        System.arraycopy(array, index, array, index + 1, size - index);
+        array[index] = val;
+        size++;
+        return true;
+    }
+
+    public int remove(int index) {
+        validIndex(index);
+        int removed = array[index];
+
+        System.arraycopy(array, index + 1, array, index, size - index - 1);
+        size--;
+        return removed;
+    }
+
+    private void resize() {
+        int newCap = array.length * RESIZE_FACTOR;
+        if (newCap < 0) throw new OutOfMemoryError("too large");
+        array = Arrays.copyOf(array, array.length * RESIZE_FACTOR);
+    }
+
+    private void validIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private void validIndexForAdd(int index) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+
 }
