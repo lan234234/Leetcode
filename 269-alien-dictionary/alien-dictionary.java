1b5162c64aa5ec37335b887016f553a26c6d6eb2
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-03 21:31:40 -0700

    Time: 3 ms (98.04%) | Memory: 43.4 MB (96.80%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,55 +1,55 @@
 class Solution {
+    Map<Character, List<Character>> graph;
+    StringBuilder sb;
+
     public String alienOrder(String[] words) {
-        // Step 0: Create data structures and find all unique letters.
-        Map<Character, List<Character>> adjList = new HashMap<>();
-        Map<Character, Integer> counts = new HashMap<>();
+        graph = new HashMap<>();
+        sb = new StringBuilder();
+
+        // 1. get all letters
         for (String word : words) {
-            for (char c : word.toCharArray()) {
-                counts.put(c, 0);
-                adjList.put(c, new ArrayList<>());
+            for (int i = 0; i < word.length(); i++) {
+                graph.putIfAbsent(word.charAt(i), new ArrayList<>());   
             }
         }
 
-        // Step 1: Find all edges.
+        // 2. construct graph
         for (int i = 0; i < words.length - 1; i++) {
-            String word1 = words[i];
-            String word2 = words[i + 1];
-            // Check that word2 is not a prefix of word1.
-            if (word1.length() > word2.length() && word1.startsWith(word2)) {
-                return "";
-            }
-            // Find the first non match and insert the corresponding relation.
-            for (int j = 0; j < Math.min(word1.length(), word2.length()); j++) {
-                if (word1.charAt(j) != word2.charAt(j)) {
-                    adjList.get(word1.charAt(j)).add(word2.charAt(j));
-                    counts.put(word2.charAt(j), counts.get(word2.charAt(j)) + 1);
+            String s1 = words[i];
+            String s2 = words[i + 1];
+            if (s2.length() < s1.length() && s1.startsWith(s2))   return "";
+            for (int j = 0; j < s1.length() && j < s2.length(); j++) {
+                char c1 = s1.charAt(j);
+                char c2 = s2.charAt(j);
+                if (c1 != c2) {
+                    graph.get(c1).add(c2);
                     break;
                 }
             }
         }
 
-        // Step 2: Breadth-first search.
-        StringBuilder sb = new StringBuilder();
-        Queue<Character> queue = new LinkedList<>();
-        for (Character c : counts.keySet()) {
-            if (counts.get(c).equals(0)) {
-                queue.add(c);
-            }
-        }
-        while (!queue.isEmpty()) {
-            Character c = queue.remove();
-            sb.append(c);
-            for (Character next : adjList.get(c)) {
-                counts.put(next, counts.get(next) - 1);
-                if (counts.get(next).equals(0)) {
-                    queue.add(next);
-                }
-            }
+        // 3. dfs
+        boolean[] visited = new boolean[26];
+        boolean[] inPath = new boolean[26];
+        for (char key : graph.keySet()) {
+            if (!isValid(key, visited, inPath)) return "";
         }
 
-        if (sb.length() < counts.size()) {
-            return "";
+        return sb.reverse().toString();
+    }
+
+    private boolean isValid(char c, boolean[] visited, boolean[] inPath) {
+        int index = c - 'a';
+        if (inPath[index])    return false;
+        if (visited[index])   return true;
+
+        inPath[index] = true;
+        for (char nei : graph.get(c)) {
+            if (!isValid(nei, visited, inPath)) return false;
         }
-        return sb.toString();
+        visited[index] = true;
+        sb.append(c);
+        inPath[index] = false;
+        return true;
     }
 }
