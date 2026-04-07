9eda1543440b2c4afd228e0c24e14e031baa6873
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-06 17:32:21 -0700

    Time: 7 ms (86.52%) | Memory: 46.3 MB (94.89%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -7,55 +7,23 @@ class Solution {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
 
-        List<String> list = new ArrayList<>();
+        PriorityQueue<String> minHeap = new PriorityQueue<>((a, b) -> (compare(a, b)));
         for (String s : map.keySet()) {
-            list.add(s);
+            minHeap.offer(s);
+            if (minHeap.size() > k) minHeap.poll();
         }
 
-        quickSelect(list, 0, list.size() - 1, k);
-        List<String> res = new ArrayList<>(list.subList(0, k));
-        Collections.sort(res, (a, b) -> (compare(a, b)));
+        List<String> res = new LinkedList<>();
+        while (!minHeap.isEmpty()) {
+            res.add(0, minHeap.poll());
+        }
         return res;
     }
 
     private int compare(String a, String b) {
         int count1 = map.get(a);
         int count2 = map.get(b);
-        if (count1 == count2)   return a.compareTo(b);
-        return count2 - count1;
-    }
-
-    private void quickSelect(List<String> list, int left, int right, int k) {
-        if (left == right)  return;
-
-        int pivotInd = (int) (Math.random() * (right - left + 1) + left);
-        String p = list.get(pivotInd);
-        swap(list, pivotInd, right);
-        int i = left;
-        int j = right - 1;
-        while (i <= j) {
-            if (compare(list.get(i), p) <= 0) {
-                i++;
-            } else if (compare(list.get(j), p) > 0) {
-                j--;
-            } else {
-                swap(list, i++, j--);
-            }
-        }
-        swap(list, i, right);
-
-        int len = i - left + 1;
-        if (len == k || len - 1 == k)   return;
-        if (len < k) {
-            quickSelect(list, i + 1, right, k - len);
-        } else {
-            quickSelect(list, left, i - 1, k);
-        }
-    }
-
-    private void swap(List<String> list, int i, int j) {
-        String temp = list.get(i);
-        list.set(i, list.get(j));
-        list.set(j, temp);
+        if (count1 == count2)   return b.compareTo(a);
+        return count1 - count2;
     }
 }
