198083141971f74282064b18e3577879a148349c
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-07 07:56:32 -0700

    Time: 5 ms (74.88%) | Memory: 44.3 MB (66.27%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.java
+++ 269-alien-dictionary/alien-dictionary.java
@@ -1,55 +1,50 @@
 class Solution {
-    Map<Character, List<Character>> graph;
-    StringBuilder sb;
-
+    Map<Character, List<Character>> map;
     public String alienOrder(String[] words) {
-        graph = new HashMap<>();
-        sb = new StringBuilder();
-
-        // 1. get all letters
+        map = new HashMap<>();
+        Map<Character, Integer> degree = new HashMap<>();
         for (String word : words) {
             for (int i = 0; i < word.length(); i++) {
-                graph.putIfAbsent(word.charAt(i), new ArrayList<>());   
+                char c = word.charAt(i);
+                map.putIfAbsent(c, new ArrayList<>());
+                degree.putIfAbsent(c, 0);
             }
         }
 
-        // 2. construct graph
         for (int i = 0; i < words.length - 1; i++) {
             String s1 = words[i];
             String s2 = words[i + 1];
-            if (s2.length() < s1.length() && s1.startsWith(s2))   return "";
-            for (int j = 0; j < s1.length() && j < s2.length(); j++) {
+            if (s2.length() < s1.length() && s1.startsWith(s2)) return "";
+            for (int j = 0; j < s1.length(); j++) {
                 char c1 = s1.charAt(j);
                 char c2 = s2.charAt(j);
                 if (c1 != c2) {
-                    graph.get(c1).add(c2);
+                    map.get(c1).add(c2);
+                    degree.put(c2, degree.get(c2) + 1);
                     break;
                 }
             }
         }
 
-        // 3. dfs
-        boolean[] visited = new boolean[26];
-        boolean[] inPath = new boolean[26];
-        for (char key : graph.keySet()) {
-            if (!isValid(key, visited, inPath)) return "";
+        StringBuilder sb = new StringBuilder();
+        Queue<Character> q = new LinkedList<>();
+        for (Map.Entry<Character, Integer> entry : degree.entrySet()) {
+            if (entry.getValue() == 0) {
+                q.offer(entry.getKey());
+            }
         }
 
-        return sb.reverse().toString();
-    }
-
-    private boolean isValid(char c, boolean[] visited, boolean[] inPath) {
-        int index = c - 'a';
-        if (inPath[index])    return false;
-        if (visited[index])   return true;
-
-        inPath[index] = true;
-        for (char nei : graph.get(c)) {
-            if (!isValid(nei, visited, inPath)) return false;
+        while (!q.isEmpty()) {
+            Character cur = q.poll();
+            sb.append(cur);
+            for (char nei : map.get(cur)) {
+                int deg = degree.get(nei) - 1;
+                if (deg == 0) {
+                    q.offer(nei);
+                }
+                degree.put(nei, deg);
+            }
         }
-        visited[index] = true;
-        sb.append(c);
-        inPath[index] = false;
-        return true;
+        return sb.length() == degree.size() ? sb.toString() : "";
     }
 }
