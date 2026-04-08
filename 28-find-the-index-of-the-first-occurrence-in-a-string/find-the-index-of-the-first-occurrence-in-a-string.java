ae1625976d986b0e0c90ce920385ef7fed43ea4c
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-08 16:09:22 -0700

    Time: 1 ms (46.84%) | Memory: 42.8 MB (93.48%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,55 +1,46 @@
 class Solution {
-    /**
-    
-    Substring,  All Proper Prefixes,    All Suffixes,   Longest Match,  LPS Value
-    A,          (None),                 (None),         None,           0
-    AB,         A,                      B,              None,           0
-    ABA,        A, AB,                  A, BA,          A (len 1),      1
-    ABAB,       A,AB,ABA                B,AB,BAB        AB (len 2)      2
-    ABABA,      A,AB,ABA,ABAB           A,BA,ABA,BABA   ABA (len 3)     3
+    private static final int FACTOR = 10003;
+    private static final int MOD = 10000007;
+    int N;
 
-    The LPS array tells: "If you fail a match here, don't start over from the beginning."
-    Imagine you are matching ABABA against a long text and you fail right after the last A.
-    - Because before the last A, the LPS value there is 2, the algorithm knows the last 2 characters you matched (AB) are also the start of your pattern.
-    - It keeps those 32 characters "matched" and just checks what comes after the first AB. You effectively "skipped" re-checking those letters.
-     */
     public int strStr(String haystack, String needle) {
-        int n = haystack.length();
-        int m = needle.length();
-        if (n < m)  return -1;
+        N = needle.length();
+        if (haystack == null || needle == null || needle.isEmpty() || haystack.length() < N)    return -1;
 
-        // 1. construct lps
-        int[] lps = new int[m];
-        int i = 0;
-        int j = 1;
-        while (j < m) {
-            if (needle.charAt(i) == needle.charAt(j)) {
-                lps[j] = ++i;    // lps[j] = ++i; can not be lps[j] = lps[j - 1] + 1;
-                j++;
-            } else if (i != 0) {
-                // utilize lps to find the comparison start point
-                i = lps[i - 1];  // can not be lps[j - 1], because the number of overlap is determined by the position i not j
-            } else {
-                j++;
-            }
-        }
-
-        // 2. utilize lps to compare
-        i = 0;
-        j = 0;
-        while (j < n && n - j >= m - i) {
-            if (needle.charAt(i) == haystack.charAt(j)) {
-                i++;
-                j++;
-                if (i == m) {
-                    return j - m;
-                }
-            } else if (i != 0) {
-                i = lps[i - 1];  // can not be lps[j - 1]
-            } else {
-                j++;
-            }
+        long hash = getHash(needle);
+        long curHash = getHash(haystack);
+        long deduct = deduct();
+        for (int i = needle.length(); i <= haystack.length(); i++) {
+            int start = i - needle.length();
+            if (hash == curHash && same(haystack, start, needle)) return start;
+            if (i == haystack.length()) break;
+            curHash = (curHash - deduct * haystack.charAt(start) % MOD + MOD) % MOD * FACTOR % MOD + haystack.charAt(i);
+            curHash %= MOD;
         }
         return -1;
+    }
+
+    private long getHash(String s) {
+        long hash = 0;
+        for (int i = 0; i < N; i++) {
+            hash = (hash * FACTOR % MOD + s.charAt(i)) % MOD;
+        }
+        return hash;
+    }
+
+    private long deduct() {
+        long res = 1L;
+        for (int i = 1; i < N; i++) {
+            res = res * FACTOR % MOD;
+        }
+        return res;
+    }
+
+    private boolean same(String s1, int start, String s2) {
+        if (start + s2.length() > s1.length())    return false;
+        for (int i = 0; i < s2.length(); i++) {
+            if (s1.charAt(start + i) != s2.charAt(i))   return false;
+        }
+        return true;
     }
 }
