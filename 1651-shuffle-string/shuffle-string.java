5b6b43341e2611ed6fb91bf59ae3e8ff4f30f997
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-08 18:22:26 -0700

    Time: 1 ms (65.62%) | Memory: 45.8 MB (29.24%) - LeetSync

--- 1651-shuffle-string/shuffle-string.java
+++ 1651-shuffle-string/shuffle-string.java
@@ -1,10 +1,31 @@
 class Solution {
+    // 0 1 2 3 4 5 6 7
+    // l e d e c o e t
+    // 0 2 6 7 4 1 1 3
+
+
+
     public String restoreString(String s, int[] indices) {
         int n = indices.length;
-        char[] arr = new char[n];
+        char[] arr = s.toCharArray();
         for (int i = 0; i < n; i++) {
-            arr[indices[i]] = s.charAt(i);
+            while (indices[i] != i) {
+                swap(arr, i, indices[i]);
+                swap(indices, i, indices[i]);
+            }
         }
         return new String(arr);
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
