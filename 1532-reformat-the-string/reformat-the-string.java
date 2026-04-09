7ff0a0466f549d3b9fc00dd855a55f58b743ed33
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-09 10:04:08 -0700

    Time: 5 ms (48.78%) | Memory: 46.6 MB (30.49%) - LeetSync

---
+++ 1532-reformat-the-string/reformat-the-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String reformat(String s) {
+        List<Character> digs = new ArrayList<>();
+        List<Character> letts = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > '9') {
+                letts.add(c);
+            } else {
+                digs.add(c);
+            }
+        }
+
+        if (Math.abs(digs.size() - letts.size()) > 1)   return "";
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        if (digs.size() < letts.size()) {
+            sb.append(letts.get(j++));
+        }
+        while (j < letts.size()) {
+            sb.append(digs.get(i++));
+            sb.append(letts.get(j++));
+        }
+        if (i < digs.size()) {
+            sb.append(digs.get(i));
+        }
+        return sb.toString();
+    }
+}
