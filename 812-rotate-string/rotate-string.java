26ad90b060e75735f04e44c81973dc1dc653a224
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-08 17:56:46 -0700

    Time: 1 ms (24.16%) | Memory: 42.6 MB (89.20%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,21 +1,32 @@
 class Solution {
+    // a b a b c
+    // 0 0 1 2 
     public boolean rotateString(String s, String goal) {
         if (s == null && goal == null)  return true;
         if (s == null || goal == null || s.length() != goal.length())  return false;
         
         int n = s.length();
-        int start1;
-        for (int start2 = 0; start2 < n; start2++) {
-            start1 = 0;
-            int i = start2;
-            while (start1 < n) {
-                if (s.charAt(start1) == goal.charAt(i)) {
-                    i = (i + 1) % n;
-                    start1++;
-                } else {
-                    break;
-                }
-                if (start1 == n)    return true;
+        String s2 = s + s;
+
+        int[] lps = new int[n];
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (goal.charAt(i) == goal.charAt(j)) {
+                lps[j] = i + 1;
+            } else if (i != 0) {
+                i = lps[i - 1];
+                j--;
+            }
+        }
+
+        i = 0;
+        for (int j = 1; j < 2 * n; j++) {
+            if (goal.charAt(i) == s2.charAt(j)) {
+                i++;
+                if (i == n) return true;
+            } else if (i != 0) {
+                i = lps[i - 1];
+                j--;
             }
         }
         return false;
