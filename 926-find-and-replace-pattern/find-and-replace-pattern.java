ad50d39bba67cd38f5e40a0dcec4e8136baaac5c
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-10 09:29:00 -0700

    Time: 0 ms (100.00%) | Memory: 43.6 MB (82.02%) - LeetSync

--- 926-find-and-replace-pattern/find-and-replace-pattern.java
+++ 926-find-and-replace-pattern/find-and-replace-pattern.java
@@ -1,11 +1,9 @@
 class Solution {
     int[] map;
-    int[] reverse;
 
     public List<String> findAndReplacePattern(String[] words, String pattern) {
         List<String> res = new ArrayList<>();
         map = new int[26];
-        reverse = new int[26];
         
         for (String word : words) {
             if (isValid(word, pattern)) {
@@ -17,14 +15,18 @@ class Solution {
 
     private boolean isValid(String word, String pattern) {
             Arrays.fill(map, -1);
-            Arrays.fill(reverse, -1);
             for (int i = 0; i < word.length(); i++) {
                 int cInd = pattern.charAt(i) - 'a';
                 int toInd = word.charAt(i) - 'a';
                 if (map[cInd] != -1 && map[cInd] != toInd)  return false;
-                if (reverse[toInd] != -1 && reverse[toInd] != cInd) return false;
                 map[cInd] = toInd;
-                reverse[toInd] = cInd;
+            }
+
+            int[] count = new int[26];
+            for (int val : map) {
+                if (val == -1)   continue;
+                if (count[val] == 1)    return false;
+                count[val] = 1;
             }
             return true;
     }
