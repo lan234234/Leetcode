58f2dde080fb6e133db79845b5280a6054e395a1
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-10 11:08:10 -0700

    Time: 2 ms (28.81%) | Memory: 45.2 MB (83.44%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        if (n < 2)  return n;
+
+        int start = 0;
+        int i = 0;
+        for (int j = 0; j <= n; j++) {
+            if (j == n || chars[j] != chars[start]) {
+                chars[i++] = chars[start];
+                int count = j - start;
+                if (count > 1) {
+                    String sCount = count + "";
+                    for (int k = 0; k < sCount.length(); k++) {
+                        chars[i++] = sCount.charAt(k);
+                    }
+                }
+                start = j;
+            }
+        }
+        return i;
+    }
+}
