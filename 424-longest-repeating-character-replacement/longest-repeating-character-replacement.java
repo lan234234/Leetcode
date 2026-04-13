ac44621b4236ec67fb6a072caafcf5fd24e53462
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-13 11:56:32 -0700

    Time: 10 ms (58.46%) | Memory: 46.3 MB (54.55%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -15,29 +15,21 @@ class Solution {
         int curMaxFreq = 0;
         int[] freq = new int[26];
         int left = 0;
+        int right;
 
-        for (int right = 0; right < s.length(); right++) {
+        for (right = 0; right < s.length(); right++) {
             int index = s.charAt(right) - 'A';
             freq[index]++;
             curMaxFreq = Math.max(curMaxFreq, freq[index]);
-            while (right - left + 1 - curMaxFreq > k) {
+            if (right - left + 1 - curMaxFreq > k) {
                 int leftIndex = s.charAt(left++) - 'A';
                 freq[leftIndex]--;
-                if (freq[leftIndex] == curMaxFreq - 1) {
-                    curMaxFreq = findMax(freq);
-                }
+                longest = Math.max(longest, right - left + 1);
             }
-            longest = Math.max(longest, right - left + 1);
         }
-        return longest;
-    }
 
-    private int findMax(int[] freq) {
-        int max = 0;
-        for(int f : freq) {
-            max = Math.max(max, f);
-        }
-        return max;
+        longest = Math.max(longest, right - left);
+        return longest;
     }
 
 }
