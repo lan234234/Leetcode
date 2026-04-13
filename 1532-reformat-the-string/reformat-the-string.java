2ce722f1bb719dc72345f83e5c24bf239d8444f7
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-13 09:46:21 -0700

    Time: 3 ms (86.45%) | Memory: 44.1 MB (99.10%) - LeetSync

--- 1532-reformat-the-string/reformat-the-string.java
+++ 1532-reformat-the-string/reformat-the-string.java
@@ -1,31 +1,48 @@
 class Solution {
     public String reformat(String s) {
-        List<Character> digs = new ArrayList<>();
-        List<Character> letts = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isDigit(c)) {
-                digs.add(c);
-            } else {
-                letts.add(c);
+        char[] arr = s.toCharArray();
+        int firstDig = -1;
+        int firstLet = -1;
+        int i;
+        for (i = 1; i < arr.length; i++) {
+            if (Character.isDigit(arr[i - 1]) == Character.isDigit(arr[i])) {
+                if (Character.isDigit(arr[i])) {
+                    firstLet = Math.max(firstLet, i + 1);
+                    while (firstLet < arr.length && Character.isDigit(arr[firstLet])) {
+                        firstLet++;
+                    }
+                    if (firstLet == arr.length) break;
+                    swap(arr, i, firstLet);
+                    firstLet++;
+                } else {
+                    firstDig = Math.max(firstDig, i + 1);
+                    while (firstDig < arr.length && Character.isLetter(arr[firstDig])) {
+                        firstDig++;
+                    }
+                    if (firstDig == arr.length) break;
+                    swap(arr, i, firstDig);
+                    firstDig++;
+                }
             }
         }
 
-        if (Math.abs(digs.size() - letts.size()) > 1)   return "";
+        if (i == arr.length)    return new String(arr);
+        if (i != arr.length - 1 || (Character.isDigit(arr[0]) == Character.isDigit(arr[i])))    return "";
+        oneShift(arr);
+        return new String(arr);
+    }
 
-        StringBuilder sb = new StringBuilder();
-        int i = 0;
-        int j = 0;
-        if (letts.size() > digs.size()) {
-            sb.append(letts.get(j++));
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private void oneShift(char[] arr) {
+        char temp = arr[arr.length - 1];
+        for (int i = arr.length - 2; i >= 0; i--) {
+            arr[i + 1] = arr[i];
         }
-        while (j < letts.size()) {
-            sb.append(digs.get(i++));
-            sb.append(letts.get(j++));
-        }
-        if (i < digs.size()) {
-            sb.append(digs.get(i));
-        }
-        return sb.toString();
+        arr[0] = temp;
     }
 }
