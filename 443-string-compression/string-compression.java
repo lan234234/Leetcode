106f55bd3d93d7238c54532fe12cc250e66cbc68
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-13 07:36:44 -0700

    Time: 1 ms (99.90%) | Memory: 45.2 MB (91.20%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,18 +1,18 @@
 class Solution {
     public int compress(char[] chars) {
         int i = 0;
-        int end = 0;
-        while (end < chars.length) {
-            int start = end;
-            while (end < chars.length && chars[end] == chars[start]) {
-                end++;
-            }
-            int count = end - start;
-            chars[i++] = chars[start];
-            if (count == 1) continue;
-            String countS = Integer.toString(count);
-            for (int j = 0; j < countS.length(); j++) {
-                chars[i++] = countS.charAt(j);
+        int start = 0;
+        for (int end = 0; end <= chars.length; end++) {
+            if (end == chars.length || chars[end] != chars[start]) {
+                int count = end - start;
+                chars[i++] = chars[start];
+                if (count != 1) {
+                    String countS = Integer.toString(count);
+                    for (int j = 0; j < countS.length(); j++) {
+                        chars[i++] = countS.charAt(j);
+                    }
+                }
+                start = end;
             }
         }
         return i;
