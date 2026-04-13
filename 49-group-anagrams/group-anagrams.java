b3b5652661a78af3bf0914c29087333b30cdfc73
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-13 10:21:04 -0700

    Time: 9 ms (33.24%) | Memory: 49.5 MB (76.56%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -4,17 +4,27 @@ class Solution {
         Map<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String sortedS = new String(arr);
-            List<String> list = map.get(sortedS);
-            if (list == null) {
-                list = new ArrayList<>();
-                map.put(sortedS, list);
-            }
-            list.add(s);
+            String compressedS = compress(s);
+            map.putIfAbsent(compressedS, new ArrayList<>());
+            map.get(compressedS).add(s);
         }
 
         return new ArrayList<>(map.values());
+    }
+
+    private String compress(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] != 0) {
+                sb.append((char) (i + 'a'));
+                sb.append(freq[i]);
+            }
+        }
+        return sb.toString();
     }
 }
