f7416428b16275a5321bb5decdbe7c6b76978e11
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-14 11:01:22 -0700

    Time: 2 ms (99.95%) | Memory: 45.4 MB (86.59%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -3,31 +3,26 @@ class Solution {
         if (s.length() < t.length())    return "";
         int start = -1;
         int end = -1;
-        Map<Character, Integer> freq = new HashMap<>();
-        int match = 0;
+        int[] freq = new int[128];
+        int notMatch = t.length();
 
         for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            freq.put(c, freq.getOrDefault(c, 0) + 1);
+            freq[t.charAt(i)]++;
         }
 
         int left = 0;
         for (int right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
-            Integer count = freq.get(c);
-            if (count == null)  continue;
-            freq.put(c, --count);
-            if (count == 0)   match++;
-            while (match == freq.size()) {
+            freq[c]--;
+            if (freq[c] >= 0)   notMatch--;
+            while (notMatch == 0) {
                 if (start == -1 || (end - start > right - left)) {
                     start = left;
                     end = right;
                 }
-                char cur = s.charAt(left++);
-                Integer leftCount = freq.get(cur);
-                if (leftCount == null)  continue;
-                freq.put(cur, ++leftCount);
-                if (leftCount == 1)   match--;
+                c = s.charAt(left++);
+                freq[c]++;
+                if (freq[c] == 1)   notMatch++;
             }
         }
         return start == -1 ? "" : s.substring(start, end + 1);
