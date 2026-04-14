1017d51cd20d3da9dc55472751699918e64ed170
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-14 08:00:49 -0700

    Time: 1 ms (23.72%) | Memory: 42.6 MB (77.37%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,18 +1,14 @@
 class Solution {
-    // a b a b c
-    // 0 0 1 2 
     public boolean rotateString(String s, String goal) {
-        if (s == null && goal == null)  return true;
-        if (s == null || goal == null || s.length() != goal.length())  return false;
-        
-        int n = s.length();
-        String s2 = s + s;
+        if (s.length() != goal.length())    return false;
 
-        int[] lps = new int[n];
+        String conS = s + s;
+        int[] lps = new int[goal.length()];
         int i = 0;
-        for (int j = 1; j < n; j++) {
-            if (goal.charAt(i) == goal.charAt(j)) {
-                lps[j] = i + 1;
+
+        for (int j = 1; j < goal.length(); j++) {
+            if (goal.charAt(j) == goal.charAt(i)) {
+                lps[j] = ++i;
             } else if (i != 0) {
                 i = lps[i - 1];
                 j--;
@@ -20,10 +16,10 @@ class Solution {
         }
 
         i = 0;
-        for (int j = 1; j < 2 * n; j++) {
-            if (goal.charAt(i) == s2.charAt(j)) {
+        for (int j = 0; j < conS.length(); j++) {
+            if (goal.charAt(i) == conS.charAt(j)) {
                 i++;
-                if (i == n) return true;
+                if (i == goal.length()) return true;
             } else if (i != 0) {
                 i = lps[i - 1];
                 j--;
@@ -31,5 +27,4 @@ class Solution {
         }
         return false;
     }
-
 }
