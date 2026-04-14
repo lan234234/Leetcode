bfa5274ad339437892dca10327662f0beb74a3f7
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-14 07:38:38 -0700

    Time: 10 ms (58.58%) | Memory: 46.4 MB (42.07%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,29 +1,20 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int longest = 0;
-        int curMaxFreq = 0;
+        int longest = Math.min(s.length(), k);
         int[] freq = new int[26];
+        int maxFreq = 0;
         int left = 0;
         int right;
-
         for (right = 0; right < s.length(); right++) {
             int index = s.charAt(right) - 'A';
             freq[index]++;
-            curMaxFreq = Math.max(curMaxFreq, freq[index]);
-            // only when we encounter invalid case, update longest
-            // because current invalid means before process current right, it was a valid sequence with maxFreq + k
-            // then only when have larger maxFreq, it is possible to have a longer sequence
-            // so we don't need to worry about the update when the sequence is acutually invalid
-            if (right - left + 1 - curMaxFreq > k) {
-                int leftIndex = s.charAt(left++) - 'A';
-                freq[leftIndex]--;
-                longest = Math.max(longest, curMaxFreq + k);
+            maxFreq = Math.max(maxFreq, freq[index]);
+            if (right - left + 1 - maxFreq > k) {
+                longest = Math.max(longest, right - left);
+                freq[s.charAt(left++) - 'A']--;
             }
         }
-
-        // not consider in the loop
         longest = Math.max(longest, right - left);
         return longest;
     }
-
 }
