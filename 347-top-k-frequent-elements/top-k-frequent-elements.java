e92f3ec47ae6a351e424c943f02d3b1fe511523d
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-14 12:34:10 -0700

    Time: 13 ms (80.07%) | Memory: 47.8 MB (36.69%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -6,18 +6,48 @@ class Solution {
             freq.put(num, freq.getOrDefault(num, 0) + 1);
         }
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> (Integer.compare(freq.get(a), freq.get(b))));
+        int[] arr = new int[freq.size()];
+        int i = 0;
         for (int key : freq.keySet()) {
-            minHeap.offer(key);
-            if (minHeap.size() > k) {
-                minHeap.poll();
-            }
+            arr[i++] = key;
         }
 
-        int[] res = new int[k];
-        for (int i = 0; i < k; i++) {
-            res[i] = minHeap.poll();
+        quickSelect(arr, 0, arr.length - 1, k);
+
+        return Arrays.copyOfRange(arr, 0, k);
+    }
+
+    private void quickSelect(int[] arr, int left, int right, int k) {
+        if (right - left + 1 == k)  return;
+
+        int pivotInd = (int) (Math.random() * (right - left + 1) + left);
+        int pFreq = freq.get(arr[pivotInd]);
+        swap(arr, pivotInd, right);
+        int i = left;
+        int j = right - 1;
+        while (i <= j) {
+            if (freq.get(arr[i]) > pFreq) {
+                i++;
+            } else if (freq.get(arr[j]) <= pFreq) {
+                j--;
+            } else {
+                swap(arr, i++, j--);
+            }
         }
-        return res;
+        swap(arr, right, i);
+
+        int len = i - left;
+        if (len == k)   return;
+        if (len < k) {
+            quickSelect(arr, i, right, k - len);
+        } else {
+            quickSelect(arr, left, i - 1, k);
+        }
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
