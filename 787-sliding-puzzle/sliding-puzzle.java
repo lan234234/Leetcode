74ae9abd43febade0286aeb8f2b2ea22cee57664
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-15 11:17:58 -0700

    Time: 7 ms (92.61%) | Memory: 44.4 MB (94.19%) - LeetSync

--- 787-sliding-puzzle/sliding-puzzle.java
+++ 787-sliding-puzzle/sliding-puzzle.java
@@ -1,6 +1,36 @@
 class Solution {
+    /**
+    input               output
+    int[][] board       int moves
+    [[1,2,3],
+     [4,5,0]]           0
+    [[1,2,0],
+     [4,5,3]]           1
+    
+    
+    assume board is not null/empty
+    assume board is 2 * 3
+    assume board only includes 0-5, each number appears only once
+    
+    bfs
+    int[][] dirs: dirs[i] represent the next position of element in i
+    dirs[0] = {1, 3}
+    dirs[1] = {0, 2, 4}
+    board -> 1d array -> "123450"
+    [[1,2,0],
+     [4,5,3]]   -> [1,2,0,4,5,3]    -> [120453] -> move first element -> nei
+                                                -> move second element
+     */
+    
+    int[][] dirs;
     public int slidingPuzzle(int[][] board) {
-        int[][] dirs = new int[][]{
+        String TARGET = "123450";
+        String initial = convertToString(board);
+
+        // corner case
+        if (TARGET.equals(initial)) return 0;
+
+        dirs = new int[][]{
             {1, 3},
             {0, 2, 4},
             {1, 5},
@@ -8,42 +38,47 @@ class Solution {
             {1, 3, 5},
             {2, 4}
         };
-        String TARGET = "123450";
-        StringBuilder sb = new StringBuilder();
         Queue<String> q = new LinkedList<>();
         Set<String> visited = new HashSet<>();
-        int step = 0;
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                sb.append(board[i][j]);
-            }
-        }
-        q.offer(sb.toString());
-        visited.add(sb.toString());
+        int move = 0;
+        q.offer(initial);
+        visited.add(initial);
 
         while (!q.isEmpty()) {
             int size = q.size();
             for (int i = 0; i < size; i++) {
-                String cur = q.poll();
-                if (TARGET.equals(cur)) return step;
-                int index = cur.indexOf('0');
-                for (int nextP : dirs[index]) {
-                    String next = generateNext(cur, index, nextP);
-                    if (visited.contains(next)) continue;
-                    q.offer(next);
-                    visited.add(next);
+                String s = q.poll();
+                if (s.equals(TARGET))   return move;
+                int j = s.indexOf('0');
+                char[] arr = s.toCharArray();
+                for (int next : dirs[j]) {
+                    swap(arr, j, next);
+                    String cur = new String(arr);
+                    if (!visited.contains(cur)) {
+                        q.offer(cur);
+                        visited.add(cur);
+                    }
+                    swap(arr, j, next);
                 }
             }
-            step++;
+            move++;
         }
         return -1;
     }
 
-    private String generateNext(String s, int i, int j) {
-        StringBuilder sb = new StringBuilder(s);
-        sb.setCharAt(i, s.charAt(j));
-        sb.setCharAt(j, s.charAt(i));
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    private String convertToString(int[][] board) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j< 3; j++) {
+                sb.append(board[i][j]);
+            }
+        }
         return sb.toString();
     }
 }
