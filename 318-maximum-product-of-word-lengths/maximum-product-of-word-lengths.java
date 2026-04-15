cbf7d25f33423a4cc7ad50bad97564468a55925f
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-15 09:54:39 -0700

    Time: 10 ms (68.89%) | Memory: 48.6 MB (93.06%) - LeetSync

--- 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
@@ -2,11 +2,15 @@ class Solution {
     public int maxProduct(String[] words) {
         int max = 0;
         int n = words.length;
+        int[] bitMask = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            bitMask[i] = getBit(words[i]);
+        }
+
         for (int i = 0; i < n - 1; i++) {
-            int bit = getBit(words[i]);
             for (int j = i + 1; j < n; j++) {
-                int curBit = getBit(words[j]);
-                if ((bit & curBit) == 0) {
+                if ((bitMask[i] & bitMask[j]) == 0) {
                     max = Math.max(max, words[i].length() * words[j].length());
                 }
             }
