b661fe1fcdb4dc724215a974d5765f6491a16db7
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-15 11:41:27 -0700

    Time: 1 ms (99.92%) | Memory: 72 MB (55.30%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,20 +1,32 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] prefixProd = new int[n];
-        int[] suffixProd = new int[n];
+        int zeroIndex = -1;
+        int zeroCount = 0;
 
-        prefixProd[0] = 1;
-        suffixProd[n - 1] = 1;
-        for (int i = 1; i < n; i++) {
-            prefixProd[i] = prefixProd[i - 1] * nums[i - 1];
-        }
-        for (int i = n - 2 ; i >= 0; i--) {
-            suffixProd[i] = suffixProd[i + 1] * nums[i + 1];
-        }
-
+        long prod = 1L;
         for (int i = 0; i < n; i++) {
-            nums[i] = prefixProd[i] * suffixProd[i];
+            if (nums[i] == 0) {
+                zeroCount++;
+                if (zeroCount > 1) {
+                    break;
+                } else {
+                    zeroIndex = i;
+                }    
+            } else {
+                prod *= nums[i];
+            }
+        }
+
+        if (zeroCount > 0)   Arrays.fill(nums, 0);
+
+        if (zeroCount == 1) {
+            nums[zeroIndex] = (int) prod;
+            return nums;
+        } else if (zeroCount == 0) {
+            for (int i = 0; i < n; i++) {
+                nums[i] = (int) (prod / nums[i]);
+            }
         }
         return nums;
     }
