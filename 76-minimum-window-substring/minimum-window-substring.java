0ff9d707710dfa1c751fbf8c25d52ca1521359d4
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-15 10:08:35 -0700

    Time: 2 ms (99.95%) | Memory: 45.5 MB (80.59%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,22 +1,43 @@
 class Solution {
+    /**
+    input                   output
+    String s    String t    String minimum window substring
+    "a"         "a"         "a"
+    "a"         "aa"        ""
+    "aa"        "a"         "a"
+    "aba"       "ab"        "ab"/"ba"
+    "abc"       "ac"        "abc"
+    
+    assume neither s nor t is null
+    assume neither s nor t is empty
+    
+    solution: sliding window
+    1. t -> char freq array
+    2. s = "ADOBECODEBANC", t = "ABC"
+                  
+
+     */
     public String minWindow(String s, String t) {
-        if (s.length() < t.length())    return "";
+        int m = s.length();
+        int n = t.length();
+        if (m < n)  return "";
+
+        int[] freq = new int[128];
+        int notMatch = n;
+        int left = 0;
         int start = -1;
         int end = -1;
-        int[] freq = new int[128];
-        int notMatch = t.length();
 
-        for (int i = 0; i < t.length(); i++) {
+        for (int i = 0; i < n; i++) {
             freq[t.charAt(i)]++;
         }
 
-        int left = 0;
-        for (int right = 0; right < s.length(); right++) {
+        for (int right = 0; right < m; right++) {
             char c = s.charAt(right);
             freq[c]--;
             if (freq[c] >= 0)   notMatch--;
             while (notMatch == 0) {
-                if (start == -1 || (end - start > right - left)) {
+                if (start == -1 || end - start > right - left) {
                     start = left;
                     end = right;
                 }
@@ -25,6 +46,7 @@ class Solution {
                 if (freq[c] == 1)   notMatch++;
             }
         }
+
         return start == -1 ? "" : s.substring(start, end + 1);
     }
 }
