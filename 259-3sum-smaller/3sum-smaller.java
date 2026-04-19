beda64480ab0798b832f14e6e595ade638cac633
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-19 16:42:44 -0700

    Time: 6 ms (99.67%) | Memory: 45.5 MB (37.17%) - LeetSync

--- 259-3sum-smaller/3sum-smaller.java
+++ 259-3sum-smaller/3sum-smaller.java
@@ -4,28 +4,19 @@ class Solution {
         int count = 0;
         Arrays.sort(nums);
         for (int i = 0; i + 2 < n; i++) {
-            for (int j = i + 1; j + 1 < n; j++) {
-                int sum = nums[i] + nums[j];
-                if (nums[j + 1] > target - sum) break;
-                int k = findLargestSmaller(nums, j + 1, target - sum);
-                count += k == -1 ? 0 : (k - j);
+            int j = i + 1;
+            if (nums[i] + nums[j] + nums[j + 1] > target)   break;
+            int k = n - 1;
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum < target) {
+                    count += k - j;
+                    j++;
+                } else {
+                    k--;
+                }
             }
         }
         return count;
-    }
-
-    private int findLargestSmaller(int[] nums, int left, int target) {
-        int res = -1;
-        int right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left)/ 2;
-            if (nums[mid] < target) {
-                res = mid;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return res;
     }
 }
