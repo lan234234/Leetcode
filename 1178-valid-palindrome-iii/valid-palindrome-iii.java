c4b88cd729005f15ef7cd059e134c276807cbdb6
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-19 11:38:57 -0700

    Time: 10 ms (89.55%) | Memory: 47.4 MB (54.23%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.java
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.java
@@ -1,26 +1,27 @@
 class Solution {
     int[][] dp;
+
     public boolean isValidPalindrome(String s, int k) {
         int n = s.length();
         dp = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+        for (int[] arr : dp) {
+            Arrays.fill(arr, -1);
         }
-        return minRemove(s, 0, n - 1) <= k;
+
+        return minRemove(s, 0, s.length() - 1) <= k;
     }
 
-    private int minRemove(String s, int start, int end) {
-        if (dp[start][end] != -1) return dp[start][end];
+    private int minRemove(String s, int i, int j) {
+        if (j < i)  return 0;
+        if (dp[i][j] != -1) return dp[i][j];
 
-        if (end == start) {
-            dp[start][end] = 0;
-        } else if (end == start + 1) {
-            dp[start][end] = s.charAt(start) == s.charAt(end) ? 0 : 1;
-        } else if (s.charAt(start) == s.charAt(end)) {
-            dp[start][end] = minRemove(s, start + 1, end - 1);
+        if (i == j) {
+            dp[i][j] = 0;
+        } else if (s.charAt(i) == s.charAt(j)) {
+            dp[i][j] = minRemove(s, i + 1, j - 1);
         } else {
-            dp[start][end] = 1 + Math.min(minRemove(s, start + 1, end), minRemove(s, start, end - 1));
+            dp[i][j] = Math.min(minRemove(s, i, j - 1), minRemove(s, i + 1, j)) + 1;
         }
-        return dp[start][end];
+        return dp[i][j];
     }
 }
