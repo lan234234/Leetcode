da47cdaa2e73cb45304d302ce434383cb061ea8e
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-20 11:58:03 -0700

    Time: 6 ms (97.62%) | Memory: 45.8 MB (48.92%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,30 +1,36 @@
 class Solution {
     List<List<Integer>> res;
+    List<Integer> cur;
     public List<List<Integer>> fourSum(int[] nums, int target) {
         res = new ArrayList<>();
+        cur = new ArrayList<>();
         int n = nums.length;
         Arrays.sort(nums);
 
-        kSum(nums, 0, target, 4, new ArrayList<>());
+        kSum(nums, 0, target, 4);
         return res;
     }
 
-    private void kSum(int[] nums, int start, long target, int k, List<Integer> cur) {
+    private void kSum(int[] nums, int start, long target, int k) {
         if (nums.length - start < k)    return;
+
+        double ave = (double) target / k;
+        if (nums[start] > ave || nums[nums.length - 1] < ave)   return;
+
         if (k == 2) {
-            twoSum(nums, start, target, cur);
+            twoSum(nums, start, target);
             return;
         }
 
         for (int i = start; i + k - 1 < nums.length; i++) {
             if (i != start && nums[i] == nums[i - 1])   continue;
             cur.add(nums[i]);
-            kSum(nums, i + 1, target - nums[i], k - 1, cur);
+            kSum(nums, i + 1, target - nums[i], k - 1);
             cur.remove(cur.size() - 1);
         }
     }
 
-    private void twoSum(int[] nums, int start, long target, List<Integer> cur) {
+    private void twoSum(int[] nums, int start, long target) {
         if (nums[start] + nums[start + 1] > target)   return;
 
         int left = start;
