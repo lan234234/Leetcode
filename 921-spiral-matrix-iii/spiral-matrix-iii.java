06db7e6be4bb6ba457b23fc14ba4a0254f57ecbb
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-22 16:48:40 -0700

    Time: 6 ms (10.86%) | Memory: 47.1 MB (64.14%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,46 +1,27 @@
 class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int[][] matrix = new int[rows][cols];
         int max = rows * cols;
         int[][] res = new int[max][2];
         int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int step = 1;
+        int cycle = 2;
         
         int r = rStart, c = cStart;
         int dirIndex = 0;
         int count = 0;
 
-        // Mark starting position
-        matrix[r][c] = 1;
-        res[count++] = new int[]{r, c};
-
         while (count < max) {
-            // Find next position logic
-            int nextR = r + dirs[dirIndex][0];
-            int nextC = c + dirs[dirIndex][1];
-
-            // Loop until we find a valid move based on your rules
-            // if next position is out of bound -> stay here + turn dir
-            // if it has been filled -> back to origianl pos + keep original dir
-            while (!isValid(nextR, nextC, rows, cols) || matrix[nextR][nextC] != 0) {
-                if (!isValid(nextR, nextC, rows, cols)) {
-                    r = nextR;
-                    c = nextC;
-                    dirIndex = (dirIndex + 1) % 4;
-                } else {
-                    dirIndex = (dirIndex + 3) % 4;
+            for (int i = 0; i < cycle; i++) {
+                for (int j = 0; j < step; j++) {
+                    if (isValid(r, c, rows, cols)) {
+                        res[count++] = new int[]{r, c};
+                    }
+                    r += dirs[dirIndex][0];
+                    c += dirs[dirIndex][1];
                 }
-                nextR = r + dirs[dirIndex][0];
-                nextC = c + dirs[dirIndex][1];
+                dirIndex = (dirIndex + 1) % 4;
             }
-
-            // Move to the found position
-            r = nextR;
-            c = nextC;
-            dirIndex = (dirIndex + 1) % 4;
-
-            // Mark and store
-            matrix[r][c] = 1;
-            res[count++] = new int[]{r, c};
+            step++;
         }
         return res;
     }
