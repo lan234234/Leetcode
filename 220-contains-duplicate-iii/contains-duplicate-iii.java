0ca4a1373bebb8d0536517df6a9b28b0ead3c498
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-22 09:26:54 -0700

    Time: 57 ms (98.07%) | Memory: 121.1 MB (43.81%) - LeetSync

--- 220-contains-duplicate-iii/contains-duplicate-iii.java
+++ 220-contains-duplicate-iii/contains-duplicate-iii.java
@@ -1,19 +1,20 @@
 class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-        // it make sure max_val_diff in each bucket within valueDiff
-        int bucketSize = valueDiff + 1;
-        Map<Long, Long> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            // make sure remappedNum is a positive integer
-            long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
-            long bucket = remappedNum / bucketSize;
-            if (map.containsKey(bucket) || 
-            (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= valueDiff) ||
-            (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= valueDiff))   return true;
-            if (map.size() == indexDiff) {
-                map.remove(((long) nums[i - indexDiff] - Integer.MIN_VALUE) / bucketSize);
+        int n = nums.length;
+        int delta = 1000000000;
+        // key: bucket index
+        Map<Integer, Integer> map = new HashMap<>();
+        int left = 0;
+        for (int right = 0; right < n; right++) {
+            int bucketIndex = (nums[right] + delta) / (valueDiff + 1);
+            if (map.containsKey(bucketIndex) || 
+                (map.containsKey(bucketIndex + 1) && map.get(bucketIndex + 1) - nums[right] <= valueDiff) ||
+                (map.containsKey(bucketIndex - 1) && nums[right] - map.get(bucketIndex - 1) <= valueDiff))
+                return true;
+            map.put(bucketIndex, nums[right]);
+            if (map.size() > indexDiff) {
+                map.remove((nums[left++] + delta) / (valueDiff + 1));
             }
-            map.put(bucket, remappedNum);
         }
         return false;
     }
