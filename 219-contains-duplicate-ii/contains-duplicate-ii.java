024bf0d77ea38ff249ae8ff9c9e12d7f5d87d5f0
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-22 09:40:31 -0700

    Time: 23 ms (82.14%) | Memory: 90.4 MB (40.60%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -3,13 +3,8 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         int left = 0;
         for (int right = 0; right < nums.length; right++) {
-            if (map.containsKey(nums[right]))  return true;
-            map.put(nums[right], 1);
-            if (right - left == k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0)   map.remove(nums[left]);
-                left++;
-            }
+            if (map.containsKey(nums[right]) && (right - map.get(nums[right]) <= k))    return true;
+            map.put(nums[right], right);
         }
         return false;
     }
