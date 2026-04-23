cf2096795a37f732d094a125d1f7b78c2a92718d
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-23 07:39:20 -0700

    Time: 2 ms (36.27%) | Memory: 43.2 MB (5.79%) - LeetSync

--- 1127-last-stone-weight/last-stone-weight.java
+++ 1127-last-stone-weight/last-stone-weight.java
@@ -2,17 +2,34 @@ class Solution {
     public int lastStoneWeight(int[] stones) {
         if (stones.length <= 1)  return 1;
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int[] freq = new int[1001];
         for (int s : stones) {
-            maxHeap.offer(s);
+            freq[s]++;
         }
 
-        while (maxHeap.size() > 1) {
-            int diff = maxHeap.poll() - maxHeap.poll();
-            if (diff > 0) {
-                maxHeap.offer(diff);
+        int rem = 0;
+        for (int i = freq.length - 1; i > 0; i--) {
+            if (i == 8) {
+                int a = 0;
+            }
+            if (freq[i] == 0)   continue;
+            if (rem == 0) {
+                freq[i] = freq[i] % 2;
+                if (freq[i] == 0)   continue;
+                rem = i;
+                freq[i] = 0;
+            } else {
+                freq[i]--;
+                int diff = rem - i;
+                if (diff >= i) {
+                    rem = diff;
+                } else {
+                    freq[diff]++;
+                    rem = 0;
+                }
+                i++;
             }
         }
-        return maxHeap.isEmpty() ? 0 : maxHeap.peek();
+        return rem;
     }
 }
