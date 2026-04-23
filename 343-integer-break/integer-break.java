81adb0b692cf509a25cf3b1a118a6903ebb3514f
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-23 16:49:40 -0700

    Time: 0 ms (100.00%) | Memory: 42.2 MB (39.82%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,12 +1,17 @@
 class Solution {
+    int[] dp;
     public int integerBreak(int n) {
-        int[] dp = new int[n + 1];
+        dp = new int[n + 1];
         dp[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            for (int cut = 1; cut <= i / 2; cut++) {
-                dp[i] = Math.max(cut * Math.max(i - cut, dp[i - cut]), dp[i]);
-            }
+        return maxBreak(n);
+    }
+
+    private int maxBreak(int n) {
+        if (dp[n] != 0) return dp[n];
+
+        for (int cut = 1; cut <= n / 2; cut++) {
+            dp[n] = Math.max(cut * Math.max(n - cut, maxBreak(n - cut)), dp[n]);
         }
-        return dp[n];
+        return dp[n]; 
     }
 }
