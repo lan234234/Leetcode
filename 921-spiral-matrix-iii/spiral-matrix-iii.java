af5f736bc3f022222e374909a881574d18b2eb41
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-23 07:22:18 -0700

    Time: 4 ms (78.10%) | Memory: 47.1 MB (44.14%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,32 +1,27 @@
 class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        int r = rStart;
+        int c = cStart;
         int max = rows * cols;
-        int[][] res = new int[max][2];
-        int[][] dirs = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        int step = 1;
-        int cycle = 2;
-        
-        int r = rStart, c = cStart;
-        int dirIndex = 0;
+        int[][] res = new int[max][];
         int count = 0;
+        int step = 1;
+        int dirInd = 0;
 
         while (count < max) {
-            for (int i = 0; i < cycle; i++) {
-                for (int j = 0; j < step; j++) {
-                    if (isValid(r, c, rows, cols)) {
+            for (int cycle = 0; cycle < 2; cycle++) {
+                for (int i = 0; i < step; i++) {
+                    if (r >= 0 && r < rows && c >= 0 && c < cols) {
                         res[count++] = new int[]{r, c};
                     }
-                    r += dirs[dirIndex][0];
-                    c += dirs[dirIndex][1];
+                    r += dirs[dirInd][0];
+                    c += dirs[dirInd][1];
                 }
-                dirIndex = (dirIndex + 1) % 4;
+                dirInd = (dirInd + 1) % 4;
             }
             step++;
         }
         return res;
-    }
-
-    private boolean isValid(int r, int c, int rows, int cols) {
-        return r >= 0 && r < rows && c >= 0 && c < cols;
     }
 }
