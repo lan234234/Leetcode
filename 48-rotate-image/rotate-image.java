845320d114feb3b4178aae3a595b079d40d558ca
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-22 17:36:58 -0700

    Time: 0 ms (100.00%) | Memory: 43.8 MB (54.99%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,19 +1,32 @@
 class Solution {
-    /**
-    [r, c] -> [c, n - 1 - r] -> [n - 1 - r, n - 1 - c] -> [n - 1 - c, r]
-    [1, 0]    [0, 2]    [2, 3]  [3,1]
-    [r, c]
-     */
+    int[][] matrix;
+    int n;
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        for (int r = 0; r < n / 2; r++) {
-            for (int c = r; c < n - 1 - r; c++) {
-                int temp = matrix[r][c];
-                matrix[r][c] = matrix[n - 1 - c][r];
-                matrix[n - 1 - c][r] = matrix[n - 1 - r][n - 1 - c];
-                matrix[n - 1 - r][n - 1 - c] = matrix[c][n - 1 - r];
-                matrix[c][n - 1 - r] = temp;
+        this.matrix = matrix;
+        n = matrix.length;
+        diagnalSwap();
+        middleSwap();
+    }
+
+    private void middleSwap() {
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n / 2; c++) {
+                swap(r, c, r, n - 1 - c);
             }
         }
+    }
+
+    private void diagnalSwap() {
+        for (int r = 0; r < n; r++) {
+            for (int c = r + 1; c < n; c++) {
+                swap(r, c, c, r);
+            }
+        }
+    }
+
+    private void swap(int r1, int c1, int r2, int c2) {
+        int temp = matrix[r1][c1];
+        matrix[r1][c1] = matrix[r2][c2];
+        matrix[r2][c2] = temp;
     }
 }
