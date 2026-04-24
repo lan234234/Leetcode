9c00a6e346799c91de2b1c972160c21657c0135e
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-24 16:59:39 -0700

    Time: 1 ms (100.00%) | Memory: 43.9 MB (56.66%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,36 +1,33 @@
 class Solution {
+    /**
+    1.1
+    1.0
+    1.e
+    
+     */
     public boolean isNumber(String s) {
-        List<Map<String, Integer>> list = new ArrayList<>();
-        list.add(Map.of("digit", 1, "sign", 2, "dot", 3));
-        list.add(Map.of("e", 5, "dot", 4, "digit", 1));
-        list.add(Map.of("digit", 1, "dot", 3));
-        list.add(Map.of("digit", 4));
-        list.add(Map.of("digit", 4, "e", 5));
-        list.add(Map.of("digit", 7, "sign", 6));
-        list.add(Map.of("digit", 7));
-        list.add(Map.of("digit", 7));
-        Set<Integer> numberState = new HashSet(List.of(1, 4, 7));
+        boolean hasE = false;
+        boolean hasDigit = false;
+        boolean hasDot = false;
 
-        int state = 0;
         for (int i = 0; i < s.length(); i++) {
-            String op;
             char c = s.charAt(i);
-            if (c == '.') {
-                op = "dot";
+            if (c == '-' || c == '+') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') return false;
+                continue;
+            } else if (c == '.') {
+                if (hasDot || hasE) return false;
+                hasDot = true;
             } else if (c == 'e' || c == 'E') {
-                op = "e";
+                if (hasE || !hasDigit)  return false;
+                hasE = true;
+                hasDigit = false;
             } else if (Character.isDigit(c)) {
-                op = "digit";
-            } else if (c == '+' || c == '-') {
-                op = "sign";
+                hasDigit = true;
             } else {
                 return false;
             }
-
-            Integer next = list.get(state).get(op);
-            if (next == null)   return false;
-            state = next;
         }
-        return numberState.contains(state);
+        return hasDigit;
     }
 }
