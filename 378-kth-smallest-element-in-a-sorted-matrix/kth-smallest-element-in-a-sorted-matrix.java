e2721f31de968041e77d60481886c9c6d3af7196
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-28 12:44:35 -0700

    Time: 17 ms (50.05%) | Memory: 51.9 MB (38.26%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -2,21 +2,18 @@ class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(matrix[a[0]][a[1]], matrix[b[0]][b[1]]));
-        boolean[][] visited = new boolean[n][n];
-        int[][] dirs = new int[][]{{0, 1}, {1, 0}};
 
-        minHeap.offer(new int[]{0, 0});
-        visited[0][0] = true;
+        int min = Math.min(k, n);
+        for (int r = 0; r < min; r++) {
+            minHeap.offer(new int[]{r, 0});
+        }
+
         while (k > 1) {
             int[] cur = minHeap.poll();
             k--;
-            for (int[] dir : dirs) {
-                int r = dir[0] + cur[0];
-                int c = dir[1] + cur[1];
-                if (r < n && c < n && !visited[r][c]) {
-                    minHeap.offer(new int[]{r, c});
-                    visited[r][c] = true;
-                }
+            cur[1]++;
+            if (cur[1] < n) {
+                minHeap.offer(cur);
             }
         }
         int[] cur = minHeap.peek();
