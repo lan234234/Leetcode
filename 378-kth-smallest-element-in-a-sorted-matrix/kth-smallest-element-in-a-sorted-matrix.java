279a9eb30a491a05a8ee85300fdef3a01649f105
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-28 12:51:48 -0700

    Time: 0 ms (100.00%) | Memory: 51.6 MB (49.08%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,22 +1,35 @@
 class Solution {
+    int n;
     public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(matrix[a[0]][a[1]], matrix[b[0]][b[1]]));
+        n = matrix.length;
 
-        int min = Math.min(k, n);
-        for (int r = 0; r < min; r++) {
-            minHeap.offer(new int[]{r, 0});
-        }
-
-        while (k > 1) {
-            int[] cur = minHeap.poll();
-            k--;
-            cur[1]++;
-            if (cur[1] < n) {
-                minHeap.offer(cur);
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
+        int res = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (countSmaller(matrix, mid) >= k) {
+                right = mid - 1;
+            } else {
+                res = mid;
+                left = mid + 1;
             }
         }
-        int[] cur = minHeap.peek();
-        return matrix[cur[0]][cur[1]];
+        return res;
+    }
+
+    private int countSmaller(int[][] matrix, int target) {
+        int r = 0;
+        int c = n - 1;
+        int count = 0;
+        while (r < n && c >= 0) {
+            if (matrix[r][c] < target) {
+                count += c + 1;
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return count;
     }
 }
