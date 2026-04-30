48eae7cb0e46496be3ea748b13fce324b4ebd830
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-29 17:00:02 -0700

    Time: 17 ms (97.41%) | Memory: 45.3 MB (75.18%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,35 +1,41 @@
 class Solution {
+    /**
+    [1,3,5,4,7]
+     1 2 3 3 
+    max = 3
+    count = 2
+    
+     */
     public int findNumberOfLIS(int[] nums) {
         int n = nums.length;
-        // dp[i]: longest subsequence end at i
-        int[] dp = new int[n];
-        // counts[i]: count longest subsequence end at i
-        int[] counts = new int[n];
-        int longest = 0;
-        int count = 0;
+        if (n == 1) return 1;
 
+        int max = 0;
+        int res = 0;
+        int[] longest = new int[n];
+        int[] count = new int[n];
         for (int i = 0; i < n; i++) {
-            dp[i] = 1;
-            counts[i] = 1;
+            longest[i] = 1;
+            count[i] = 1;
             for (int j = i - 1; j >= 0; j--) {
                 if (nums[i] > nums[j]) {
-                    int curLen = dp[j] + 1;
-                    if (curLen > dp[i]) {
-                        dp[i] = curLen;
-                        counts[i] = counts[j];
-                    } else if (curLen == dp[i]) {
-                        counts[i] += counts[j];
+                    int curLen = longest[j] + 1;
+                    if (curLen > longest[i]) {
+                        longest[i] = curLen;
+                        count[i] = count[j];
+                    } else if (curLen == longest[i]) {
+                        count[i] += count[j];
                     }
                 }
             }
-            if (longest == dp[i]) {
-                count += counts[i];
-            } else if (longest < dp[i]) {
-                longest = dp[i];
-                count = counts[i];
-            }
-        }
 
-        return count;
+            if (longest[i] > max) {
+                max = longest[i];
+                res = count[i];
+            } else if (longest[i] == max) {
+                res += count[i];
+            } 
+        }
+        return res;
     }
 }
