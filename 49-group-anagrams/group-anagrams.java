3a66a19d5a36d836883e830d4347df00305af76a
lan234234 <104956570+lan234234@users.noreply.github.com>
2026-04-30 10:37:33 -0700

    Time: 8 ms (39.08%) | Memory: 49.6 MB (73.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,28 +1,42 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> res = new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
+    /**
+    1. sort -> group
+    sort: llogl * n
+    map: l * n
 
+    2. map
+    word -> char map-> string    l
+
+    
+    
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-            String compressedS = compress(s);
-            map.putIfAbsent(compressedS, new ArrayList<>());
-            map.get(compressedS).add(s);
+            String sortedS = sort(s);
+            map.putIfAbsent(sortedS, new ArrayList<>());
+            map.get(sortedS).add(s);
         }
 
-        return new ArrayList<>(map.values());
+        List<List<String>> list = new ArrayList<>();
+        for (List<String> cur : map.values()) {
+            list.add(cur);
+        }
+        return list;
     }
 
-    private String compress(String s) {
+    private String sort(String s) {
         int[] freq = new int[26];
+
         for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 26; i++) {
+        for (int i = 0; i < freq.length; i++) {
             if (freq[i] != 0) {
-                sb.append((char) (i + 'a'));
                 sb.append(freq[i]);
+                sb.append((char) (i + 'a'));
             }
         }
         return sb.toString();
